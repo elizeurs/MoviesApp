ec24cc77cdfc6caa6ba678805d4d64e65980c5c9
elizeurs <13ders@gmail.com>
2021-04-28 15:45:35 -0300

    Implementing MovieDetailViewModel

---
+++ MoviesApp/View Models/MovieDetailViewModel.swift
@@ -0,0 +1,64 @@
+//
+//  MovieDetailViewModel.swift
+//  MoviesApp
+//
+//  Created by Elizeu RS on 28/04/21.
+//  Copyright © 2021 Mohammad Azam. All rights reserved.
+//
+
+import Foundation
+
+class MovieDetailViewModel: ObservableObject {
+  
+  private var movieDetail: MovieDetail?
+  @Published var loadingState = LoadingState.loading
+  
+  private var httpClient = HTTPClient()
+  
+  init(movieDetail: MovieDetail? = nil) {
+    self.movieDetail = movieDetail
+  }
+  
+  func getDetailsByImdbId(imdbId: String) {
+    
+    httpClient.getMovieDetailsBy(imdb: imdbId) { result in
+      switch result {
+      case .success(let details):
+        // whenever you're trying to set a "@Published" property, make sure you do it on the main thread.
+        DispatchQueue.main.async {
+          self.movieDetail = details
+          self.loadingState = .success
+        }
+        
+      case .failure(let error):
+        print(error.localizedDescription)
+        DispatchQueue.main.async {
+          self.loadingState = .failed
+        }
+      }
+    }
+  }
+  
+  var title: String {
+    self.movieDetail?.title ?? ""
+  }
+  
+  var poster: String {
+    self.movieDetail?.poster ?? ""
+  }
+  
+  var plot: String {
+    self.movieDetail?.poster ?? ""
+  }
+  
+  var rating: Int {
+    get {
+      let ratingAsDouble = Double(self.movieDetail?.imdbRating ?? "0.0")
+      return Int(ceil(ratingAsDouble ?? 0.0))
+    }
+  }
+  
+  var director: String {
+    return movieDetail?.director ?? ""
+  }
+}
