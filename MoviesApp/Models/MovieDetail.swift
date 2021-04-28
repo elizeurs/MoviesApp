eff82f7dfc72eba4e8ced850a38a744d01105b72
elizeurs <13ders@gmail.com>
2021-04-28 13:08:58 -0300

    Implementing MovieDetail Model

---
+++ MoviesApp/Models/MovieDetail.swift
@@ -0,0 +1,34 @@
+//
+//  MovieDetail.swift
+//  MoviesApp
+//
+//  Created by Elizeu RS on 28/04/21.
+//  Copyright © 2021 Mohammad Azam. All rights reserved.
+//
+
+import Foundation
+
+struct MovieDetail: Decodable {
+  
+  let title: String
+  let year: String
+  let rated: String
+  let plot: String
+  let director: String
+  let actors: String
+  let imdbRating: String
+  let poster: String
+  let imdbId: String
+  
+  private  enum CodingKeys: String, CodingKey {
+    case title = "Title"
+    case year = "Year"
+    case poster = "Poster"
+    case rated = "Rated"
+    case plot = "Plot"
+    case director = "Director"
+    case actors = "Actors"
+    case imdbRating = "imdbRating"
+    case imdbId = "imdbID"
+  }
+}
